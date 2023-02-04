7260b4911517473202fff58845c9eb6dd6311a0e
Agustin Alexander <agustin.alexander@salesforce.com>
2023-02-04 10:49:29 -0300

    changed to alpine

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 #Deriving the latest base image
-FROM python:latest
+FROM python:3.9.16-alpine3.17
 
+RUN apk add git
 
 WORKDIR /usr/app/src
+COPY requirements.txt .
+RUN pip3 install -r requirements.txt
 COPY main.py .
-
 CMD [ "python", "./main.py"]
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import requests
 from ibflex import client, parser, FlexQueryResponse, Trade
 from datetime import datetime
 import os
+import json
 
-def get_trades():
-    ibkrtoken = os.environ.get("IBKR-TOKEN")
-    ibkrquery = os.environ.get("IBKR-QUERY")
+ghost_token = os.environ.get('GHOST_TOKEN')
+ibkrtoken = os.environ.get("IBKR-TOKEN")
+ibkrquery = os.environ.get("IBKR-QUERY")
+account_id = os.environ.get("GHOST_ACCOUNT", "")
+ghost_host = os.environ.get("GHOST_HOST", "https://ghostfol.io")
+
+
+def sync_ibkr():
     response = client.download(ibkrtoken, ibkrquery)
     query: FlexQueryResponse = parser.parse(response)
     activities = []
@@ -16,19 +23,83 @@ def get_trades():
             symbol = trade.symbol
             if ".USD-PAXOS" in trade.symbol:
                 symbol = trade.symbol.replace(".USD-PAXOS", "") + "USD"
+            if account_id != "":
+                activities.append({
+                    "accountId": account_id,
+                    "currency": trade.currency,
+                    "dataSource": "YAHOO",
+                    "date": iso_format,
+                    "fee": float(0),
+                    "quantity": float(trade.quantity),
+                    "symbol": symbol,
+                    "type": trade.buySell,
+                    "unitPrice": float(trade.tradePrice)
+                })
+            else:
+                activities.append({
+                    "currency": trade.currency,
+                    "dataSource": "YAHOO",
+                    "date": iso_format,
+                    "fee": float(0),
+                    "quantity": float(trade.quantity),
+                    "symbol": symbol,
+                    "type": trade.buySell,
+                    "unitPrice": float(trade.tradePrice)
+                })
 
-            activities.append({
-                "accountId": "426f6dff-d019-4662-a92c-7f14b08a3235",
-                "currency": trade.currency,
-                "dataSource": "YAHOO",
-                "date": iso_format,
-                "fee": float(0),
-                "quantity": float(trade.quantity),
-                "symbol": symbol,
-                "type": trade.buySell,
-                "unitPrice": float(trade.tradePrice)
-            })
-    activities = {"activities": sorted(activities, key=lambda x: x["date"])}
-    return activities
+    for activity in sorted(activities, key=lambda x: x["date"]):
+        pushToGhostfolio({"activities": [activity]})
+
+
+def deleteAct(id):
+    url = f"{ghost_host}/api/v1/order/{id}"
+
+    payload = {}
+    headers = {
+        'Authorization': f"Bearer {ghost_token}",
+    }
+
+    response = requests.request("DELETE", url, headers=headers, data=payload)
+
+    return response.status_code == 200
+
+
+def deleteAllActs():
+    acts = getAllActs()
+    complete = True
+    for act in acts:
+        act_complete = deleteAct(act['id'])
+        complete = complete and act_complete
+        if act_complete:
+            print("Deleted: " + act['id'])
+        else:
+            print("Failed Delete: " + act['id'])
+    return complete
+
+
+def getAllActs():
+    url = "https://ghostfol.io/api/v1/order"
+
+    payload = {}
+    headers = {
+        'Authorization': f"Bearer {ghost_token}",
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.json()['activities']
+
+
+def pushToGhostfolio(acts):
+    url = "https://ghostfol.io/api/v1/import"
+    payload = json.dumps(acts)
+    headers = {
+        'Authorization': f"Bearer {ghost_token}",
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+
+
 if __name__ == '__main__':
-    get_trades()
+    sync_ibkr()
