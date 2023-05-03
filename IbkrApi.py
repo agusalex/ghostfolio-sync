a31164bb36c6035bce4b2708893948003199121c
longstone <3483450-longstone@users.noreply.gitlab.com>
2023-05-03 14:56:04 +0200

    add ibflex debug output (query to file)

--- IbkrApi.py
+++ IbkrApi.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from cachetools import cached, TTLCache
 from ibflex import client, parser, FlexQueryResponse
-from cachetools import cached, TTLCache
+
 import LoggerFactory
 from EnvironmentConfiguration import EnvironmentConfiguration
 
@@ -20,6 +20,16 @@ class IbkrApi():
     def get_and_parse_query(self):
         logger.debug("Fetching Query")
         response = client.download(self.ibkr_token, self.ibkr_query)
+        if envConf.is_debug_files_enabled():
+            self.__query_to_file(response)
         logger.debug("Parsing Query")
         query: FlexQueryResponse = parser.parse(response)
         return query
+
+    def __query_to_file(self, response):
+        timestamp = f"{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}"
+        folder = envConf.debug_file_location()
+        filename = f"{folder}{timestamp}-ib_flex_query-{self.ibkr_query}.xml"
+        logger.warning(f"writing query to {filename}")
+        with open(filename, 'wb') as outfile:
+            outfile.write(response)
